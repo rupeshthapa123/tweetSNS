bc5ec0ebac8073b300b33a5d51906613227ee20d
Rupesh <rupesh.thapa2050@gmail.com>
2022-04-15 17:06:36 +0545

    django api added and react implementation started

---
+++ tweets/serializers.py
@@ -0,0 +1,46 @@
+from importlib.resources import contents
+from django.conf import settings
+from rest_framework import serializers
+from tweetSNS.settings import TWEET_ACTION_OPTIONS
+from .models import Tweet
+
+MAX_TWEET_LENGTH = settings.MAX_TWEET_LENGTH
+TWEET_ACTION_OPTIONS = settings.TWEET_ACTION_OPTIONS
+
+class TweetActionSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    action = serializers.CharField()
+    content = serializers.CharField(allow_blank=True, required=False)
+
+    def validate_action(self, value):
+        value = value.lower().strip()
+        if not value in TWEET_ACTION_OPTIONS:
+            raise serializers.ValidationError("This is not a valid action for tweets")
+        return value
+
+class TweetCreateSerializer(serializers.ModelSerializer):
+    likes = serializers.SerializerMethodField(read_only=True)
+    
+    class Meta:
+        model = Tweet
+        fields = ['id','content','likes']
+    
+    def get_likes(self, obj):
+        return obj.likes.count()
+    
+    def validate_content(self, content):
+        if len(content) > MAX_TWEET_LENGTH:
+            raise serializers.ValidationError("This tweet is too long")
+        return content
+
+class TweetSerializer(serializers.ModelSerializer):
+    likes = serializers.SerializerMethodField(read_only=True)
+    parent = TweetCreateSerializer(read_only=True)
+    
+    class Meta:
+        model = Tweet
+        fields = ['id','content','likes','is_retweet',"parent"]
+    
+    def get_likes(self, obj):
+        return obj.likes.count()
+    
